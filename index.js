1e8eed8ae98a1ca43470e53096c973e9e071852b
Giancarlo <galliani.08@gmail.com>
2016-05-10 22:36:38 +0100

    Update
    -Streamlined JS code completely
    
    -Adjusted position of elements using CSS to make the site responsive
    
    -Removed jittering of the Tweet button when the New Quote button is generated.
    
    TO DO: Remove jittering on page load.

--- index.js
+++ index.js
@@ -1,49 +1,30 @@
-$(document).ready(function(json) {
-  $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&key=111111&format=jsonp&lang=en&jsonp=?", function(json) {
-    var quote = json["quoteText"] + " ~ " + json["quoteAuthor"];
-    if (json["quoteAuthor"] === 'Anonymous' | json["quoteAuthor"] === '') {
-      $("#mes").html(
-        "<div class='quote'><h3>" + json["quoteText"] + "</h3></div><div class='author'><h5 class='text-muted'> Anonymous</h5></div>").fadeIn();
-      $("#tweetButton").on("click", function() {
-        window.open("http://www.twitter.com/home/?status=" + quote.replace(/;/g, " --") + "Anonymous")
-      })
-    } else {
-      $("#mes").html(
-        "<div class='quote'><h3>" + json["quoteText"] + "</h3></div>" + "<div class='author'><h5 class ='text-muted'>" + json["quoteAuthor"] + "</h5></div>").fadeIn();
-      $("#tweetButton").on("click", function() {
-        window.open("http://www.twitter.com/home/?status=" + quote.replace(/;/g, "--"))
+var quote = "";
+var tweet = ""
+var getQuote = function () {
+    
+    $("#mes").fadeOut(function(){    
+    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&key=111111&format=jsonp&lang=en&jsonp=?", function (json) {
+        if (json.quoteAuthor === 'Anonymous' | json.quoteAuthor === '') {
+                quote = "<div class='quote'><h3>" + json.quoteText + "</h3></div><div class='author'><h5 class='text-muted'> Anonymous</h5></div>";
+        tweet = json.quoteText + " — Anonymous";            
+        } else {
+                quote = "<div class='quote'><h3>" + json.quoteText + "</h3></div>" + "<div class='author'><h5 class ='text-muted'>" + json.quoteAuthor + "</h5></div>"
+                tweet = json.quoteText + " — " + json.quoteAuthor;
+          };
+          $("#mes").html(quote).fadeIn();
+        $("#tweet").fadeIn()
+            
+    })
+})}
+                      
 
-      });
+$("#tweetButton1,#tweetButton2").on("click", function () {
+    window.open("http://www.twitter.com/home/?status=" + tweet.replace(/;/g, "&mdash"))
 
-      $("#quoteButton").on("click", function() {
+});
 
-        $("#mes").fadeOut(function() {
-          $("#tweet").html("<button class='btn' id='tweetButton2'><span class= 'fa fa-lg fa-twitter'><br></span> Tweet This Quote</button></div></div>");
-          $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&key=111111&format=jsonp&lang=en&jsonp=?", function(json) {
-            var quote = json["quoteText"] + " ~ " + json["quoteAuthor"];
-            if (json["quoteAuthor"] === 'Anonymous' | json["quoteAuthor"] === '') {
-              $("#mes").html(
-                "<div class='quote'><h3>" + json["quoteText"] + "</h3></div><div class='author'><h5 class='text-muted'> Anonymous</h5></div>").fadeIn();
-
-              $("#tweetButton2").on("click", function() {
-                window.open("http://www.twitter.com/home/?status=" + quote.replace(/;/g, " --") + "Anonymous")
-              });
-            } else {
-              $("#mes").html(
-                "<div class='quote'><h3>" + json["quoteText"] + "</h3></div>" + "<div class='author'><h5 class ='text-muted'>" + json["quoteAuthor"] + "</h5></div>").fadeIn();
-
-              $("#tweetButton2").on("click", function() {
-                window.open("http://www.twitter.com/home/?status=" + quote.replace(/;/g, " --"))
-              });
-
-            }
-
-          })
-
-        })
-      })
-
-    }
-
-  })
+$("#quoteButton").on("click", function(){
+$("#tweet").fadeOut(getQuote)
 })
+
+$(document).ready(getQuote);
